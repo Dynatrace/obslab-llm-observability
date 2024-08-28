bbb6bb2b95c9ba3fb89d9c5f2935a181c4a4b8b0
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-08-29 09:12:06 +1000

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,3 +3,13 @@
 # Install
 kind create cluster --config .devcontainer/kind-cluster.yml --wait 300s
 chmod +x .devcontainer/deployment.sh && .devcontainer/deployment.sh
+
+# Creation Ping
+curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"tenant\": \"$DT_ENDPOINT\",
+    \"repo\": \"$GITHUB_REPOSITORY\",
+    \"demo\": \"obslab-syslog\",
+    \"codespace.name\": \"$CODESPACE_NAME\"
+  }"
